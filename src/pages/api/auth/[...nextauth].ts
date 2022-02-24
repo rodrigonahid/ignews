e1e9259ecc054108e37a99b0ad07ac6a3821cd0c
rodrigonahid <rodrigonahid1@gmail.com>
2022-02-23 21:42:04 -0300

    integration with faundb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +19,12 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(user);
+      await fauna.query(q.Create(q.Collection("users"), { data: user.email }));
+
+      return true;
+    },
+  },
 });
